56a078d8e1b759d140a9f840e572e2f255308958
Nick Wilde <n.wilde1374@gmail.com>
2020-05-01 19:08:49 +0100

    Work on endpoints

--- src/Abstracts/AbstractRouteMiddleware.php
+++ src/Abstracts/AbstractRouteMiddleware.php
@@ -6,12 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 abstract class AbstractRouteMiddleware
 {
-    public function __construct()
-    {
-        echo 'Lol';
-    }
+    final public function __invoke(Request $request, object $next): Response {
 
-    final public function __invoke(Request $request, Response $response, callable $next): Response {
-        return $response;
     }
 }
