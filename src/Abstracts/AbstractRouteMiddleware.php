377d791cc3f1518999db2a05571f75fd48e2aa44
Nick Wilde <n.wilde1374@gmail.com>
2020-05-02 14:07:25 +0100

    Logic for hardcoded middleware

--- src/Abstracts/AbstractRouteMiddleware.php
+++ src/Abstracts/AbstractRouteMiddleware.php
@@ -1,12 +1,83 @@
 <?php
 namespace Duppy\Abstracts;
 
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 abstract class AbstractRouteMiddleware
 {
-    final public function __invoke(Request $request, object $next): Response {
+    /**
+     * Request obj
+     *
+     * @var Request
+     */
+    protected static Request $request;
 
+    /**
+     * Request Handler obj
+     *
+     * @var RequestHandler
+     */
+    protected static RequestHandler $handler;
+
+    /**
+     * Response obj
+     *
+     * @var Response
+     */
+    protected static Response $response;
+
+    /**
+     * Calls the middleware
+     *
+     * @param Request $request
+     * @param RequestHandler $handler
+     * @return Response
+     */
+    final public function __invoke(Request $request, RequestHandler $handler): Response {
+        self::$request = $request;
+        self::$handler = $handler;
+        self::$response = $handler->handle($request);
+
+        $this->handle();
+        return self::getResponse();
+    }
+
+    /**
+     * Handles the middleware
+     *
+     * @return Response
+     */
+    abstract public function handle(): Response;
+
+    /**
+     * Get request obj
+     *
+     * @return Request
+     */
+    final protected static function getRequest(): Request
+    {
+        return self::$request;
+    }
+
+    /**
+     * Get handler obj
+     *
+     * @return RequestHandler
+     */
+    final protected static function getHandler(): RequestHandler
+    {
+        return self::$handler;
+    }
+
+    /**
+     * Get response obj
+     *
+     * @return Response
+     */
+    final protected static function getResponse(): Response
+    {
+        return self::$response;
     }
 }
--- src/Middleware/TestMiddleware.php
+++ src/Middleware/TestMiddleware.php
@@ -2,8 +2,15 @@
 namespace Duppy\Middleware;
 
 use Duppy\Abstracts\AbstractRouteMiddleware;
+use Psr\Http\Message\ResponseInterface as Response;
 
 class TestMiddleware extends AbstractRouteMiddleware
 {
+    final public function handle(): Response
+    {
+        $response = self::getResponse();
+        $response->getBody()->write('Middleware!');
 
+        return $response;
+    }
 }
