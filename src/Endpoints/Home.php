c8d2221e266a59ebda41315a198f9dfa68fc8372
Ethan Jones <ethan@yasfu.net>
2020-05-28 20:28:25 -0600

    Forums Gitmodule Fix

--- src/Endpoints/Home.php
+++ src/Endpoints/Home.php
@@ -1,10 +1,24 @@
 <?php
-
-
 namespace Duppy\Endpoints;
 
 
-class Home
+use Duppy\Abstracts\AbstractEndpoint;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Home extends AbstractEndpoint
 {
+
+    /**
+     * Set the URI to / or home
+     *
+     * @var string
+     */
+    public static string $uri = "/[home]";
+
+    public function __invoke(Request $request, Response $response, array $args = []): Response
+    {
+        return $response;
+    }
 
 }
