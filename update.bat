5299b759e1bb6fce9291383127c2e1fe50dcc89c
Ethan Jones <ethan@yasfu.net>
2020-11-23 01:31:42 -0700

    update.bat for windows

---
+++ update.bat
@@ -0,0 +1,12 @@
+@ECHO off
+
+:ask
+set /P c=Do you want to update composer? [(Y) \ n]
+if /I "%c%" EQU "N" goto :skip_update_composer
+
+composer update
+
+:skip_update_composer
+vendor/bin/doctrine.exe orm:schema-tool:update --force --dump-sql
+
+PAUSE
