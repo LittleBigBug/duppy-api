10fda1bf360a41814e3b1d6d0cb8f9f6dd612948
Ethan Jones <ethan@yasfu.net>
2020-12-03 00:46:01 -0700

    teapot

---
+++ src/Endpoints/Teapot.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Duppy\Endpoints;
+
+use Duppy\Abstracts\AbstractEndpoint;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class Teapot extends AbstractEndpoint {
+
+    public static ?array $uri = null;
+
+    public function __invoke(Request $request, Response $response, array $args = []): Response {
+        return $response->withStatus(418);
+    }
+
+}
